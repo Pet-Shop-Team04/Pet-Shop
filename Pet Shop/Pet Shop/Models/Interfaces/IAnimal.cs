cfedcb4f890f7138d24ef806a1d45c99b81e4b93
Yousef Samara <yousef.aa.samara@gmail.com>
2022-05-11 20:40:52 +0300

    yousef

---
+++ Pet Shop/Pet Shop/Models/Interfaces/IAnimal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pet_Shop.Models.Interfaces
+{
+    interface IAnimal
+    {
+        Task<Animal> Create(Animal animal);
+        Task<Animal> GetAnimal(int Id);
+        Task<List<Animal>> GetAnimals();
+        Task<Animal> UpdateAnimal(int Id, Animal animal);
+        Task DeleteAnimal(int Id);
+    }
+}
---
+++ Pet Shop/Pet Shop/Models/Interfaces/IAnimalProdact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pet_Shop.Models.Interfaces
+{
+    interface IAnimalProdact
+    {
+        Task<AnimalProdact> Create(AnimalProdact animalProdact);
+        Task<AnimalProdact> GetAnimalProdact(int Id);
+        Task<List<AnimalProdact>> GetAnimalProdacts();
+        Task<AnimalProdact> UpdateAnimalProdact(int Id, AnimalProdact animalProdact);
+        Task DeleteAnimalProdact(int Id);
+    }
+}
